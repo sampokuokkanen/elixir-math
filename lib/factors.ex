3efa1a66e410dcf50ef7f8ae9a04920813581e92
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2021-03-20 23:14:38 +0900

    now with multiplication

--- lib/factors.ex
+++ lib/factors.ex
@@ -6,16 +6,24 @@ defmodule Factors do
 
 
   def run(args) do
+    {opts,_,_}= OptionParser.parse(args, [switches: [multiplication: :boolean]])
+    IO.inspect opts, label: "Command Line Arguments"
     IO.puts "Starting the 'find the factors of an integer' app!'"
     first = List.first(args)
-    IO.inspect first
     {user_int, _remainder} = unless first do
       Integer.parse(IO.gets "Please input your integer:")
     else
       Integer.parse(first)
     end
-
-    IO.inspect factors(user_int)
+    if opts[:multiplication] do
+      multiples_of(user_int)
+      |> Enum.with_index
+      |> Enum.each(fn({x, i}) ->
+        IO.puts "#{user_int} x #{i + 1} = #{x}"
+      end)
+    else
+      IO.inspect factors(user_int)
+    end
   end
 
   @doc """
@@ -30,4 +38,18 @@ defmodule Factors do
   def factors(i) do
     Enum.reject(1..i, fn x -> rem(i, x) != 0 end)
   end
+
+  @doc """
+  Multiples of a number up to 10.
+
+  ## Examples
+
+      iex> Factors.multiples_of(4)
+      [4, 8, 12, 16, 20, 24, 28, 32, 36, 40]
+
+  """
+  def multiples_of(i) do
+    Enum.map(1..10, fn x -> x * i end)
+  end
+
 end
